b0f6ed3cf71993957c15d85a2b69e3f1f0ea6255
Julie Wiltbank <juliemwiltbank@gmail.com>
2023-02-21 13:30:24 -0700

    updates to trigger functions

--- lasertag/trigger.c
+++ lasertag/trigger.c
@@ -20,6 +20,9 @@
 #define GUN_TRIGGER_PRESSED 1
 #define GUN_TRIGGER_RELEASED 0
 
+volatile static trigger_shotsRemaining_t trigger_shots_remaining;
+volatile static bool ignoreGunInput;
+
 // Trigger can be activated by either btn0 or the external gun that is attached to TRIGGER_GUN_TRIGGER_MIO_PIN
 // Gun input is ignored if the gun-input is high when the init() function is invoked.
 bool triggerPressed() {
@@ -34,34 +37,34 @@ bool triggerPressed() {
 void trigger_init() {
     mio_init(false);  // false disables any debug printing if there is a system failure during init.
     mio_setPinAsOutput(TRIGGER_GUN_TRIGGER_MIO_PIN);  // Configure the signal direction of the pin to be an output.
-    // Other stuff...
+    ignoreGunInput = false;
 }
 
 // Standard tick function.
 void trigger_tick() {
-
+    
 }
 
 // Enable the trigger state machine. The trigger state-machine is inactive until
 // this function is called. This allows you to ignore the trigger when helpful
 // (mostly useful for testing).
 void trigger_enable() {
-
+    ignoreGunInput = false;
 }
 
 // Disable the trigger state machine so that trigger presses are ignored.
 void trigger_disable() {
-
+    ignoreGunInput = true;
 }
 
 // Returns the number of remaining shots.
 trigger_shotsRemaining_t trigger_getRemainingShotCount() {
-
+    return trigger_shots_remaining;
 }
 
 // Sets the number of remaining shots.
 void trigger_setRemainingShotCount(trigger_shotsRemaining_t count) {
-
+    trigger_shots_remaining = count;
 }
 
 // Runs the test continuously until BTN3 is pressed.
