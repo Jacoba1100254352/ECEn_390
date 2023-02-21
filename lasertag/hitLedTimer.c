c86855a5d06531676bcf45058ab6a94dfee5925f
Julie Wiltbank <juliemwiltbank@gmail.com>
2023-02-21 12:46:45 -0700

    updates to hitLedTimer.c

--- lasertag/hitLedTimer.c
+++ lasertag/hitLedTimer.c
@@ -20,22 +20,58 @@
 #define WRITE_LED_0_OFF 0
 #define MIO_LED_ON 1
 #define MIO_LED_OFF 0
+#define STATE_UPDATE_ERR_MSG "Error in state update"
+#define STATE_ACTION_ERR_MSG "Error in state action"
+
+// States for the controller state machine.
+volatile enum hitLedTimer_st_t {
+	waiting_st,                 // Start here, transition out of this state on the first tick.
+	hit_st,
+    hit_over_st
+};
+volatile static enum hitLedTimer_st_t currentState;
 
 // Need to init things.
 void hitLedTimer_init() {
     mio_init(false);  // false disables any debug printing if there is a system failure during init.
     mio_setPinAsOutput(HIT_LED_MIO_PIN);  // Configure the signal direction of the pin to be an output.
+    currentState = waiting_st;
     // Other stuff...
 }
 
 // Standard tick function.
 void hitLedTimer_tick() {
+      // Perform state update first.
+  switch(currentState) {
+    case waiting_st:
+      break;
+    case hit_st:
+      break;
+    case hit_over_st:
+      break;
+    default:
+      print(STATE_UPDATE_ERR_MSG);
+      break;
+  }
+  
+  // Perform state action next.
+  switch(currentState) {
+    case waiting_st:
+      break;
+    case hit_st:
+      break;
+    case hit_over_st:
+      break;
+     default:
+      print(STATE_ACTION_ERR_MSG);
+      break;
+  }  
 
 }
 
 // Calling this starts the timer.
 void hitLedTimer_start() {
-
+    
 }
 
 // Returns true if the timer is currently running.
@@ -62,7 +98,7 @@ void hitLedTimer_disable() {
 
 // Enables the hitLedTimer.
 void hitLedTimer_enable() {
-    
+
 }
 
 // Runs a visual test of the hit LED until BTN3 is pressed.
