190cf48a519020228a244aeb839a6b73e3b12fd1
Julie Wiltbank <juliemwiltbank@gmail.com>
2023-02-21 13:26:23 -0700

    updates

--- lasertag/hitLedTimer.c
+++ lasertag/hitLedTimer.c
@@ -15,10 +15,8 @@
 #define DPCHAR(ch)
 #endif
 
-#define HIT_LED_MIO_PIN 11
 #define LED_ON 1
 #define LED_OFF 0
-#define TIMER_CYCLES 50000 // Cycles for .5s
 #define STATE_UPDATE_ERR_MSG "Error in state update"
 #define STATE_ACTION_ERR_MSG "Error in state action"
 #define LED_ON_ST_MSG "In LED_ON_ST"
@@ -38,7 +36,7 @@ volatile static enum hitLedTimer_st_t currentState;
 // Need to init things.
 void hitLedTimer_init() {
     mio_init(false);  // false disables any debug printing if there is a system failure during init.
-    mio_setPinAsOutput(HIT_LED_MIO_PIN);  // Configure the signal direction of the pin to be an output.
+    mio_setPinAsOutput(HIT_LED_TIMER_OUTPUT_PIN);  // Configure the signal direction of the pin to be an output.
     currentState = LED_OFF_ST;
     leds_init(false);
     timer_enable = true; // Unsure if true or false...
@@ -73,7 +71,7 @@ void hitLedTimer_tick() {
       // Perform state update first.
   switch(currentState) {
     case LED_ON_ST:
-        if (timer >= TIMER_CYCLES) {
+        if (timer >= HIT_LED_TIMER_EXPIRE_VALUE) {
             timer = 0;
             hitLedTimer_turnLedOff();
             currentState = LED_OFF_ST;
@@ -119,13 +117,13 @@ bool hitLedTimer_running() {
 
 // Turns the gun's hit-LED on.
 void hitLedTimer_turnLedOn() {
-    mio_writePin(HIT_LED_MIO_PIN, LED_ON);
+    mio_writePin(HIT_LED_TIMER_OUTPUT_PIN, LED_ON);
     leds_write(LED_ON);
 }
 
 // Turns the gun's hit-LED off.
 void hitLedTimer_turnLedOff() {
-    mio_writePin(HIT_LED_MIO_PIN, LED_ON);
+    mio_writePin(HIT_LED_TIMER_OUTPUT_PIN, LED_ON);
     leds_write(LED_ON);
 }
 
